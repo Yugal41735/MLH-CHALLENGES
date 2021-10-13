e3d9f07bb7156f4a1daa940a15b890f549ad75c3
Yugal41735 <yugal1434@gmail.com>
2021-10-13 19:39:20 +0530

    Added songs :)

---
+++ Replit/main.py
@@ -0,0 +1,15 @@
+from replit import web
+import flask
+app = flask.Flask(__name__)
+
+users = web.UserStore()
+
+@app.route("/")
+@web.authenticated
+def index():
+  hits = users.current.get("hits", 0) + 1
+  users.current["hits"]=hits
+  return f"You have visited here {hits} times"
+  # return "yoo whats up"
+
+web.run(app)
